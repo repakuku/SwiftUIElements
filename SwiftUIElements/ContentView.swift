81b70c0821b717bbb202186cee1ad0f10cb1513a
Alexey Turulin <repakuku@icloud.com>
2023-09-07 14:34:48 -0400

    Initial Commit

---
+++ SwiftUIElements/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIElements
+//
+//  Created by Alexey Turulin on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIElements/SwiftUIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIElementsApp.swift
+//  SwiftUIElements
+//
+//  Created by Alexey Turulin on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
