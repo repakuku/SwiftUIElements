52e4b4a8fb108e1d4d5248a5be4be80256ba49af
Alexey Turulin <repakuku@icloud.com>
2023-09-07 16:45:44 -0400

    working with swiftUI elements and modifiers

--- SwiftUIElements/ContentView.swift
+++ SwiftUIElements/ContentView.swift
@@ -20,8 +20,7 @@ struct ContentView: View {
             UserNameView(name: displayedName)
             ColorSliderView(value: $sliderValue)
             TextField("Enter your name", text: $userName)
-                .textFieldStyle(.roundedBorder)
-            
+                .bordered()
             Text("Welcome to SwiftUI")
                 .font(.system(size: 60))
                 .bold()
@@ -80,5 +79,24 @@ struct UserNameView: View {
             }
             Spacer()
         }
+    }
+}
+
+struct BorderedViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+            .overlay(
+                RoundedRectangle(cornerRadius: 4)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(color: .gray.opacity(0.4), radius: 3, x: 1, y: 2)
+    }
+}
+
+extension TextField {
+    func bordered() -> some View {
+        modifier(BorderedViewModifier())
     }
 }
